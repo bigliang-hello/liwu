333ae8fcf7a628c8155e1a39f16019b2774dbddc
bigliang_iOS <xlhelloworld@163.com>
2019-05-08 17:34:46 +0800

    ceshi

--- app/Http/Controllers/Api/ImageController.php
+++ app/Http/Controllers/Api/ImageController.php
@@ -9,6 +9,7 @@ class ImageController extends Controller
 {
     public function store(Request $request, ImageUploadHandler $uploadHandler)
     {
+        dd(111);
         $result = $uploadHandler->save($request->file, str_plural('cover'), 200);
         return $this->response->array($result);
     }
